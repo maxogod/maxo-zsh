02a191e79b7198f8c757178ac5a23bc40c73060f
maxogod <105026197+maxogod@users.noreply.github.com>
2026-08-13 23:45:48 -0300

    chore: add .local/bin to PATH and add nvim as EDITOR

--- conf.d/00-envs.zsh
+++ conf.d/00-envs.zsh
@@ -6,9 +6,12 @@ HISTSIZE=1000
 SAVEHIST=1000
 HISTFILE=~/.zsh_history
 
+export EDITOR=nvim
+
 # --- Path editions --- #
 
 export PATH="$PATH:$HOME/go/bin"
+export PATH="$PATH:$HOME/.local/bin"
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"

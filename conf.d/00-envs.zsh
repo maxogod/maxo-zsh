43bfe12df55de39e3f0a775f8d82ee726da2454e
maxogod <105026197+maxogod@users.noreply.github.com>
2026-08-30 19:17:15 -0300

    feat: conditionally add pnpm to path

--- conf.d/00-envs.zsh
+++ conf.d/00-envs.zsh
@@ -7,11 +7,15 @@ SAVEHIST=1000
 HISTFILE=~/.zsh_history
 
 export EDITOR=nvim
+export PNPM_HOME="/home/maxo/.local/share/pnpm"
 
 # --- Path editions --- #
 
 export PATH="$PATH:$HOME/go/bin"
 export PATH="$PATH:$HOME/.local/bin"
+if [ -d "$PNPM_HOME/bin" ]; then
+  export PATH="$PATH:$PNPM_HOME/bin"
+fi
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
